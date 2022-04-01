720a8828fefacf50246c435fea17c62f6e057f85
Jun Naruse <jun.naruse@gmail.com>
2022-04-01 18:32:46 -0500

    [ADD] Backend endpoints y configuracion inicial

--- back/Program.cs
+++ back/Program.cs
@@ -1,43 +1,75 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var builder = WebApplication.CreateBuilder(args); // Creates a webapplicationbuilder with preconfigured defaults
+builder.Services.AddDbContext<ArticuloDb>(opt => opt.UseInMemoryDatabase("webPage")); // Adds the database context to the dependency injection
+// builder.Services.AddDatabaseDeveloperPageExceptionFilter(); // Enables displaying database related exceptions
+var app = builder.Build(); // Creates a webapplication with preconfigured defaults
 
-var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/api/articulos", async (ArticuloDb db) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    await db.Articulos.ToListAsync();
+});
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+app.MapPost("/api/articulos", async (Articulo articuloInput, ArticuloDb db) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    // Check that code is unique
+    var articulo = await db.Articulos.FirstOrDefaultAsync(x => x.codigo == articuloInput.codigo);
 
-app.MapGet("/weatherforecast", () =>
+    if (articulo is not null) return Results.BadRequest();
+
+    db.Articulos.Add(articuloInput);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/api/articulos/{articuloInput.id}", articuloInput);
+});
+
+app.MapPut("/api/articulos/{codigo}", async (string codigo, Articulo articuloInput, ArticuloDb db) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var articulo = await db.Articulos.FirstOrDefaultAsync(x => x.codigo == codigo);
+
+    if (articulo is null) return Results.NotFound();
+    
+    articulo.codigo = articuloInput.codigo;
+    articulo.nombre = articuloInput.nombre;
+    articulo.descripcion = articuloInput.descripcion;
+    articulo.cantidad = articuloInput.cantidad;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/api/articulos/{codigo}", async (string codigo, ArticuloDb db) =>
+{
+    if (await db.Articulos.FirstOrDefaultAsync(x => x.codigo == codigo) is Articulo articulo)
+    {
+        db.Articulos.Remove(articulo);
+        await db.SaveChangesAsync();
+        return Results.Ok(articulo);
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
 
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+// Model
+class Articulo
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int id { get; set; }
+    public string codigo { get; set; } = string.Empty;
+    public string? nombre { get; set; }
+    public string? descripcion { get; set; }
+    public int cantidad { get; set; }
+}
+
+// Database context
+class ArticuloDb : DbContext
+{
+    public ArticuloDb(DbContextOptions<ArticuloDb> options)
+        : base(options) { }
+
+    public DbSet<Articulo> Articulos => Set<Articulo>();
 }
